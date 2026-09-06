64099d4547957b2447bbc5a0c6fea7fb245d5683
Noel Matero <noelbenjaminmatero@gmail.com>
2026-09-06 11:53:24 +0300

    new stuff / mainly refactor

--- src/membership/src/wire.rs
+++ src/membership/src/wire.rs
@@ -1,4 +1,4 @@
-use crate::node::{Incarnation, LocalNode, NodeId};
+use crate::node::{Incarnation, LocalNode, Member, MemberState, NodeId};
 use serde::{Deserialize, Serialize};
 use std::net::SocketAddr;
 
@@ -26,10 +26,37 @@ pub enum WireMemberState {
     Dead,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct WireMember {
     pub id: NodeId,
     pub addr: SocketAddr,
     pub incarnation: Incarnation,
     pub state: WireMemberState,
+}
+
+impl From<MemberState> for WireMemberState {
+    fn from(member_state: MemberState) -> Self {
+        match member_state {
+            MemberState::Dead => WireMemberState::Dead,
+            MemberState::Alive => WireMemberState::Alive,
+            MemberState::Suspect { since: _ } => WireMemberState::Suspect,
+        }
+    }
+}
+
+impl WireMember {
+    pub fn new(member: Member) -> WireMember {
+        WireMember {
+            id: member.id,
+            addr: member.addr,
+            incarnation: member.incarnation,
+            state: WireMemberState::from(member.state),
+        }
+    }
+}
+
+impl From<Member> for WireMember {
+    fn from(member: Member) -> Self {
+        WireMember::new(member)
+    }
 }
